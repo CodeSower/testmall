ee9a6385438dc1fc2539a649ed78ba23e78ebe17
谢苹 <18670025760@163.com>
2020-09-03 22:59:47 +0800

    tabbar引入

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-
-
+import MainTabBar from "./components/content/mainTabBar/MainTabBar";
 export default {
   name: 'App',
   components: {
-
+  MainTabBar
   }
 }
 </script>
